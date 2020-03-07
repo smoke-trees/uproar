19f9fabd6956f540ee3b26351164d7213c610001
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-08 01:43:10 +0530

    Removed passport and database instances

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ const expressLayouts=require('express-ejs-layouts');
 const mongoose=require('mongoose');
 const flash=require('connect-flash');
 const session=require('express-session');
-const passport=require('passport');
 const app=express();
-//passport Config
-require('./config/passport')(passport);
-//DB Config
-const db=require('./config/keys').MongoURI;
 app.use(expressLayouts);
-mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true})
-    .then(()=>console.log("MongoDB connceted"))
-    .catch(err=>console.log(err));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended: false}));
 //Express Session
@@ -21,8 +13,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 //Flash
 app.use(flash());
 //Global vars
