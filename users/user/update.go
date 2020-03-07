58916020f3038dc38abfb039d6f52063c60d5169
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-08 01:45:53 +0530

    Post microservices remove, credibility updation rule add
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

--- users/user/update.go
+++ users/user/update.go
@@ -16,20 +16,30 @@ type post struct {
 	rel float64
 }
 
-func updateRelAndUserOnUp(user user,post post) (user, post) {
-
-	post.rel = post.postUp - post.postDown
-
+func updateRelAndUserOnUp(user user,post post) user {
 	user.relUp = append(user.relUp, post)
 
-	return user, post
+	return user
 }
 
-func updateRelAndUserOnDown(user user,post post) (user, post) {
-
-	post.rel = post.postUp - post.postDown
-
+func updateRelAndUserOnDown(user user,post post) user {
 	user.relDown = append(user.relDown, post)
 
-	return user, post
+	return user
+}
+
+func updateCred (user user) user{
+	for _, relUp := range(user.relUp){
+		user.cred += relUp.rel
+	}
+
+	for _, relDown := range(user.relDown){
+		user.cred -= relDown.rel
+	}
+
+	for _, post := range(user.posts){
+		user.cred += post.rel
+	}
+
+	return user
 }
