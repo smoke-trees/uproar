2654743315c33b041f1c20f531edb6c135e6f094
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-03-08 01:03:43 +0530

    Removed the index Routes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req,res,next)=>{
     res.locals.error_msg=req.flash('error_msg');
     next();
 })
-app.use("/",require("./routes/index"));
+
 app.use('/users',require("./routes/users"));
 const PORT=process.env.PORT || 5000;
 
