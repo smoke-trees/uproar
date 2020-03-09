9ca07766e9fc803cb143d7a2bcd86663b40c24cb
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-09 09:09:49 +0530

    Toxic Bot Add
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

---
+++ model/Toxic/load_toxic.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 00:05:37 2020
+
+@author: Tanmay Thakur
+"""
+
+import tensorflow as tf
+from tensorflow.keras.models import load_model   
+
+
+def init():
+    model = load_model('/home/rosguy/uproar/model/toxic_model.h5')
+    graph = tf.compat.v1.get_default_graph()
+    
+    return model,graph
---
+++ model/Toxic/predict_toxic.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 00:03:34 2020
+
+@author: Tanmay Thakur
+"""
+
+import pickle
+import model.load_toxic as l
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+tokenizer = pickle.load(open("/home/rosguy/uproar/model/tokenizer_instance.pickle","rb"))
+
+model,_ = l.init()
+
+def make_sentence(sentence):
+    return [sentence]
+
+
+def preprocessing(sentence, max_features = 20000, maxlen = 50, tokenizer = tokenizer):
+    list_tokenized_train = tokenizer.texts_to_sequences(sentence)
+    
+    X_t = pad_sequences(list_tokenized_train, maxlen = maxlen)
+    return X_t
+
+def prediction(x):
+    x = preprocessing(make_sentence(x))
+    list_classes = ["Toxic", "Severely Toxic", "Obscene", "Threat", "Insult", "Identity Hate"]     
+    x = str(dict(zip(list_classes, 100*model.predict(x).flatten())))
+    return x
+
+if __name__ == "__main__":
+    print(prediction('kill'))
